12f1c85b1222235195501a601c16fda0e4cc8583
ShamsAlW <salwaeli@icloud.com>
2026-04-23 04:55:07 -0500

    running with github pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
 import Layout from "./components/Layout";
 import MapPage from "./pages/MapPage";
@@ -7,7 +7,7 @@ import ExplorePage from "./pages/ExplorePage";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<MapPage />} />
@@ -15,6 +15,6 @@ export default function App() {
           <Route path="*" element={<Navigate to="/" replace />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
